b89a72aac4ac5a373226e6101c8d223bdf333745
DESKTOP-82CIEC1\Cao Hong An <hongan@jv-it.com.vn>
2022-06-29 21:15:42 +0700

    update_bai_test

---
+++ src/components/ContactUs.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="contact-us">
+    <h2>Contact Us</h2>
+    <div class="content-input">
+        <input placeholder="Input Email" v-model="info.email" type="email"><br>
+        <textarea placeholder="Input Content..." v-model="info.content" name="" id="" cols="30" rows="10"></textarea>
+        <button class="button-css" @click="addItem" type="button">See all</button>
+    </div>  
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      info:{
+          email: null,
+          content : null,
+      }
+    };
+  },
+  mounted() {},
+  methods: {
+    addItem(){
+        alert(this.info)
+    }
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+.contact-us {
+  text-align: center;
+  padding: 0 300px;
+  .button-css{
+      background-color: rgb(72, 209, 97);
+      border : 1px solid rgb(72, 209, 97);
+      color: white;
+      border-radius: 50px;
+      padding: 10px 20px;
+      float: right;
+       opacity: 0.9;
+      &:hover{
+      
+          opacity: 1;
+      }
+  }
+  .content-input{
+      padding: 0 100px;
+      text-align: left;
+      input{
+          border: none;
+          border-radius:2px;
+          margin-bottom: 10px;
+          height: 30px;
+        width: 100%;
+          background-color: #f2f2f2;
+          &:focus{
+              outline: none !important;
+              border: white;
+          }
+      }
+      textarea{
+          border: none;
+          border-radius:2px;
+          margin-bottom: 10px;
+          width: 100%;
+          background-color: #f2f2f2;
+          &:focus{
+              outline: none !important;
+              border: white;
+          }
+      }
+  }
+
+}
+</style>
---
+++ src/components/OurProject.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="our-project">
+    <h2>Our Project</h2>
+    <div class="grid-container">
+      <div
+        v-for="(item, index) in listBox"
+        :key="index"
+        class="grid-item"
+        :style="`background-color:${random_rgba()}`"
+      ></div>
+    </div>
+    <button class="button-css" @click="addItem" type="button">See all</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      listBox: [1, 2, 3, 4, 5, 6],
+    };
+  },
+  mounted() {},
+  methods: {
+    random_rgba() {
+      var o = Math.round,
+        r = Math.random,
+        s = 255;
+      return (
+        "rgba(" +
+        o(r() * s) +
+        "," +
+        o(r() * s) +
+        "," +
+        o(r() * s) +
+        "," +
+        r().toFixed(1) +
+        ")"
+      );
+    },
+    addItem(){
+        this.listBox.push(7,8,9)
+    }
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+.our-project {
+  text-align: center;
+  padding: 0 200px;
+  .grid-container {
+    display: grid;
+    column-gap: 10px;
+    row-gap: 10px;
+    grid-template-columns: auto auto auto;
+    padding: 10px;
+    .grid-item {
+      padding: 20px;
+      font-size: 30px;
+      height: 200px;
+      text-align: center;
+      transition: 0.3s;
+      opacity: 0.5;
+      &:hover{
+        opacity: 1;
+        cursor: pointer;
+      }
+    }
+  }
+  .button-css{
+      background-color: white;
+      border : 1px solid rgb(72, 209, 97);
+      border-radius: 50px;
+      padding: 10px 20px;
+      &:hover{
+          background-color: rgb(72, 209, 97);
+          color: white;
+      }
+  }
+
+}
+</style>
